1f963c34075d2ab72ae2cb47636090b5ed5496ff
miguel10m10 <miguelangelmuro10@gmail.com>
2017-10-05 14:52:37 -0300

    Herencia of the test case

---
+++ ConsoleApp1/testt/herenciaTest/BaseTest.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.testt.herenciaTest
+{
+    class BaseTest
+    {
+
+        public IWebDriver driver = null;
+
+        [SetUp]
+        public void Initilize()
+        {
+            FirefoxDriverService service = FirefoxDriverService.CreateDefaultService(@"C:\Users\mdalessio\Desktop", "geckodriver.exe");
+            driver = new FirefoxDriver(service);
+        }
+        [TearDown]
+        public void EndTest()
+        {
+
+            driver.Quit();
+        }
+
+
+    }
+}
---
+++ ConsoleApp1/testt/herenciaTest/TestHer1.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.testt.herenciaTest
+{
+    class TestHer1 : BaseTest
+    {
+        [Test]
+        public void Test1()
+        {
+            driver.Navigate().GoToUrl("https://www.google.com");
+        }
+    }
+}
