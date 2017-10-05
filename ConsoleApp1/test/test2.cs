8a123b0d34fee01bbf2986470288b267ee7909c6
miguel10m10 <miguelangelmuro10@gmail.com>
2017-10-05 11:22:25 -0300

    test c#

--- ConsoleApp1/test/test2.cs
+++ ConsoleApp1/test/test2.cs
@@ -17,8 +17,6 @@ namespace ToolsQA
             // Find the element that's ID attribute is 'account'(My Account) 
             driver.FindElement(By.XPath(".//*[@id='account']/a")).Click();
 
-
-
             // Find the element that's ID attribute is 'log' (Username)
             // Enter Username on the element found by above desc.
             driver.FindElement(By.Id("log")).SendKeys("testuser_1");
