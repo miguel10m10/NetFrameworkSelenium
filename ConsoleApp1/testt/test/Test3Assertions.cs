199a6d9e2fb3cf6f3ea854e103f542936d6b6e10
Miguel D'Alessio <mdalessio@baufest.com>
2017-11-01 15:29:08 -0300

    Assert and Warning cases

---
+++ ConsoleApp1/testt/test/Test3Assertions.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.testt.test
+{
+    class Test3Assertions
+    {
+        IWebDriver driver = null;
+
+        [Test]
+        public void Test3Assertion()
+        {
+            //  Assert.That(2 + 2, Is.EqualTo(4));
+            // throw new Exception();
+            // Assert.Fail("Fail Test");
+            // Assert.Inconclusive("hello");
+            // Assert.Warn("This is not good");
+        }
+    }
+}
---
+++ ConsoleApp1/testt/test/test4Warning.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.testt.test
+{
+    class test4Warning
+    {
+        [Test]
+        public void Test4Warniings()
+        {
+            Warn.If(2 + 2 != 5);
+            Warn.If(2 + 2, Is.Not.EqualTo(5));
+         
+        }
+    }
+}
